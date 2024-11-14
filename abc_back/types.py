6b86f6da70e3d55546b158fa045a4f1ac96a6299
Kalykov Alisher <a.kalykov@teachbase.ru>
2024-11-14 10:19:18 +0600

    fix flake8 abc_back/types

--- abc_back/types.py
+++ abc_back/types.py
@@ -1,15 +1,1 @@
-from typing import NamedTuple, TypeVar
-
-from django.db.models import Model
-
-
-Id = int
-
-_Model = TypeVar("_Model", bound=Model)
-
-
-class ModelInfo(NamedTuple):
-    """Информация о модели в проекте."""
-
-    app_name: str
-    model_name: str
+Id = int | str
