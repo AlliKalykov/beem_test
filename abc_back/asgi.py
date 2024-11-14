59d00e7901b246b189a307e6600a702c587f88e1
Kalykov Alisher <a.kalykov@teachbase.ru>
2024-11-14 17:37:20 +0600

    fix wsgi and asgi

--- abc_back/asgi.py
+++ abc_back/asgi.py
@@ -1,4 +1,10 @@
+import os
+
 from configurations.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abc_back.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "DEV")
 
 
 application = get_asgi_application()
--- abc_back/wsgi.py
+++ abc_back/wsgi.py
@@ -1,4 +1,10 @@
+import os
+
 from configurations.wsgi import get_wsgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abc_back.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
 
 application = get_wsgi_application()
