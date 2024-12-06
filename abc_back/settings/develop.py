f7f0b8c53256ef5bb4c797901577e2b244cdb130
Kalykov Alisher <a.kalykov@teachbase.ru>
2024-12-06 15:35:06 +0600

    cors fix

--- abc_back/settings/develop.py
+++ abc_back/settings/develop.py
@@ -4,10 +4,25 @@ from .base import Base
 class Dev(Base):
     DEBUG = True
     ALLOWED_HOSTS = ["*"]
+    CSRF_TRUSTED_ORIGINS = [
+        "http://localhost:300",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://31.129.97.238:8000",
+    ]
 
     CORS_ALLOW_CREDENTIALS = True
 
     CORS_ALLOW_ORIGINS = [
+        "http://localhost:300",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://31.129.97.238:8000",
+    ]
+
+    CORS_ALLOW_HEADERS = "*"
+
+    CORS_ORIGIN_WHITELIST = [
         "http://localhost:300",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
