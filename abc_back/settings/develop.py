e5286325cee061500eee57b2523945f48fb356f6
Kalykov Alisher <a.kalykov@teachbase.ru>
2024-12-13 16:53:24 +0600

    fix sessions anas version

--- abc_back/settings/develop.py
+++ abc_back/settings/develop.py
@@ -4,29 +4,22 @@ from .base import Base, env
 class Dev(Base):
     DEBUG = True
     ALLOWED_HOSTS = ["*"]
+
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = False
+    SESSION_COOKIE_HTTPONLY = True
+
+    CSRF_COOKIE_SAMESITE = 'None'
+    CSRF_COOKIE_SECURE = False
     CSRF_TRUSTED_ORIGINS = [
-        "http://localhost:300",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://31.129.97.238:8000",
     ]
 
     CORS_ALLOW_CREDENTIALS = True
-
-    CORS_ALLOW_ORIGINS = [
-        "http://localhost:300",
+    CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://31.129.97.238:8000",
-    ]
-
-    CORS_ALLOW_HEADERS = "*"
-
-    CORS_ORIGIN_WHITELIST = [
-        "http://localhost:300",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://31.129.97.238:8000",
     ]
 
     SENTRY_DSN = env("SENTRY_DSN", default=None)
